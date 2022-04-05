1b0359144f9d7aca7651339151b9f550a0e35c7a
Daphne Castro <dc4768@nyu.edu>
2022-04-04 22:08:33 -0400

    jit

---
+++ 05-randomWords.js
@@ -0,0 +1,4 @@
+var array_adj = ["happy", "sad", "adventurous", "brave", "calm", "powerful", "successful"];
+var randomIndex = Math.floor(Math.random()*array_adj.length);
+
+console.log("Ally is " + array_adj[randomIndex]);
