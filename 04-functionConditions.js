e8059c852e719d425040ba74bc3e26187076f84d
Daphne Castro <dc4768@nyu.edu>
2022-04-04 21:35:40 -0400

    kay

---
+++ 04-functionConditions.js
@@ -0,0 +1,11 @@
+var userNum = 3
+
+function pickNumber(num) {
+  if (num == 3) {
+    console.log("You guessed the correct number!")
+  } else {
+    console.log("You guessed the wrong number!");
+  }
+}
+
+pickNumber(userNum);
