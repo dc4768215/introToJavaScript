599fc41bcaec66de2b42c1ddb642e3dd22beacf0
Daphne Castro <dc4768@nyu.edu>
2022-04-04 20:36:51 -0400

    lay

--- 01-stringCompare.js
+++ 01-stringCompare.js
@@ -4,5 +4,5 @@ var string2 = "tums";
 if (string1 === string2) {
   alert("The strings are the same.");
 } else {
-  alert("The strings are not the same.")
+  alert("The strings are not the same.");
 }
