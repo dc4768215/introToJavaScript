64a37efcd0bc6382634d85ad55f1a7a681bd2e5b
Daphne Castro <dc4768@nyu.edu>
2022-04-04 21:13:59 -0400

    hi

---
+++ 02-numberCompare.js
@@ -0,0 +1,10 @@
+var num1 = 12;
+var num2 = 3;
+
+if (num1 < num2) {
+  alert(num1 + " is less than " + num2);
+} else if (num1 === num2) {
+  alert(num1 + " is equal to " + num2);
+} else {
+  alert(num1 + " is greater than " + num2);
+}
